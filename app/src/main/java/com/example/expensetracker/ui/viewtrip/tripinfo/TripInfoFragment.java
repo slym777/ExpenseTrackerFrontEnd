b18aa5149c18478af150737c5b4aaf184b31231b
slym777 <darii.dan98@gmail.com>
2022-02-02 23:50:01 +0200

    - added list of members where is required

--- app/src/main/java/com/example/expensetracker/ui/viewtrip/tripinfo/TripInfoFragment.java
+++ app/src/main/java/com/example/expensetracker/ui/viewtrip/tripinfo/TripInfoFragment.java
@@ -12,17 +12,23 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.bumptech.glide.Glide;
 import com.example.expensetracker.R;
 import com.example.expensetracker.databinding.FragmentTripInfoBinding;
+import com.example.expensetracker.ui.viewtrip.personalexpense.PersonalExpensesAdapter;
 import com.example.expensetracker.utils.BaseApp;
+
+import java.util.ArrayList;
 
 public class TripInfoFragment extends Fragment {
 
     private TripInfoViewModel tripInfoViewModel;
+    private TripMembersAdapter tripMembersAdapter;
     private FragmentTripInfoBinding binding;
     private Long tripId;
+    private Boolean isMemberListExpanded;
 
     public TripInfoFragment(Long tripId) {
         super();
@@ -41,10 +47,30 @@ public class TripInfoFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.membersRecyclerView.setHasFixedSize(true);
+        binding.membersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        tripMembersAdapter = new TripMembersAdapter(new ArrayList<>());
+        binding.membersRecyclerView.setAdapter(tripMembersAdapter);
+
+        isMemberListExpanded = false;
+
         tripInfoViewModel.tripLive.observe(getViewLifecycleOwner(), trip -> {
             binding.descValue.setText(trip.getDescription());
             binding.locationValue.setText(trip.getLocation());
             binding.nrContributors.setText(trip.getGroupSize().toString());
+            tripMembersAdapter.updateRecyclerView(trip.getUsers());
+        });
+
+        binding.memberListExpand.setOnClickListener(v -> {
+            if (isMemberListExpanded) {
+                binding.membersRecyclerView.setVisibility(View.VISIBLE);
+                binding.imageExpand.setImageDrawable(getResources().getDrawable(R.drawable.expand2_icon));
+                isMemberListExpanded = false;
+            } else {
+                binding.membersRecyclerView.setVisibility(View.GONE);
+                binding.imageExpand.setImageDrawable(getResources().getDrawable(R.drawable.expand_icon));
+                isMemberListExpanded = true;
+            }
         });
     }
 
